f42249e6d9cfb2094fd50c1d3e0b586543bac829
André Jaenisch <andre.jaenisch@posteo.de>
2018-11-03 23:17:55 +0100

    test: harden against bogus input. Fixes #35
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- packages/lobsang-processor-hostname/__tests__/lobsang-processor-hostname.test.js
+++ packages/lobsang-processor-hostname/__tests__/lobsang-processor-hostname.test.js
@@ -3,6 +3,22 @@ import test from 'ava'
 
 import lobsangProcessorHostname from '..'
 
+test('rejects if bogus input is sent', async (t) => {
+  const url = null
+  const error = await t.throwsAsync(async () => {
+    await lobsangProcessorHostname(url)
+  })
+  t.is(error.message, 'The "url" argument must be of type string. Received type object')
+})
+
+test('rejects if not an url', async (t) => {
+  const url = 'not.an.url'
+  const error = await t.throwsAsync(async () => {
+    await lobsangProcessorHostname(url)
+  })
+  t.is(error.message, 'Not an URL')
+})
+
 test('extracts the hostname from an url', async (t) => {
   const url = 'https://my.example.com/'
   const hostname = await lobsangProcessorHostname(url)
