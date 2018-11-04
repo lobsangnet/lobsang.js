f31b31a2d4ce014258d15035f1af5b7a8d13956f
André Jaenisch <andre.jaenisch@posteo.de>
2018-11-04 23:02:57 +0100

    docs: license banner + docstrings to lobsang-connect-matrix. Refs #37
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- packages/lobsang-connect-matrix/lib/check.js
+++ packages/lobsang-connect-matrix/lib/check.js
@@ -1,9 +1,42 @@
 'use strict'
+/*
+Copyright 2018 André Jaenisch
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**
+ * @module @lobsangnet/lobsang-connect-matrix/check
+ */
+
+/**
+ * Checks the message on type etc.
+ *
+ * @private
+ * @arg {String} message - The message to send.
+ * @returns {Promise<String|Error>}
+ *   Promise which resolved to a cleaned message.
+ *   Or an Error if rejected.
+ */
 function check (message) {
   if (!message) {
     return Promise.reject(new Error('Argument must not be falsy!'))
   }
+
+  if (typeof message !== 'string') {
+    return Promise.reject(new Error('Argument must be a string!'))
+  }
+
   return Promise.resolve(message)
 }
 
--- packages/lobsang-connect-matrix/lib/client.js
+++ packages/lobsang-connect-matrix/lib/client.js
@@ -1,9 +1,46 @@
 'use strict'
+/*
+Copyright 2018 André Jaenisch
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**
+ * Matrix JS SDK
+ * @typedef {Object} SDK
+ */
+
+/**
+ * A Matrix Client.
+ * @typedef {Object} MatrixClient
+ */
+
 global.Olm = require('olm')
 const sdk = require('matrix-js-sdk')
 
 const CONSTANTS = require('./constants')
 
+/**
+ * @module @lobsangnet/lobsang-connect-matrix/client
+ */
+
+/**
+ * Instantiates a SDK client of Matrix.
+ *
+ * @private
+ * @returns {Promise<MatrixClient>}
+ *   Promise, which resolves to a MatrixClient.
+ */
 const createClient = () => {
   const client = sdk.createClient({
     baseUrl: CONSTANTS.MATRIX_HOMESERVER,
